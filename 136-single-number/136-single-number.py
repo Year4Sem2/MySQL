5da86e60e6c8380736d63bd5d358c304ff286d29
Year4Sem2 <62819100+Year4Sem2@users.noreply.github.com>
2022-07-23 11:38:23 +0800

    Delete 136-single-number directory

--- 136-single-number/136-single-number.py
+++
@@ -1,9 +0,0 @@
-
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        mylist = list(dict.fromkeys(nums))
-        for key in mylist:
-            if nums.count(key) == 1:
-                return key
-        
-        
